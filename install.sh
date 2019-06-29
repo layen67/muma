6fbdf3e899f6b4403c24ef2bee217b805149479d
layen67 <lkbcontact@gmail.com>
2019-06-30 00:42:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,3 @@
-wget 
+wget https://uc4fbd2c9772e59b1aea8e3f72ee.dl.dropboxusercontent.com/cd/0/get/AjwwZH5QJzLMsRMcQdsT3nyVzK4UjJ8sEE2WQ-ZW3mXuHdMTgBG4wfgai2RBQqDK0RQS2rWzkptHDvHsd-k1TWmWqmsEX2qMUUDeMsMPDLUqmQ/file?dl=1#
+mv * mumara.tar.gz
+tar xzvf mumara.tar.gz
